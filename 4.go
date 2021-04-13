b4ff8478762dda78b13b931a2583510fe16f7e0e
Your Name <you@example.com>
2021-04-13 21:17:45 +0600

    hw

--- 4.go
+++ 4.go
@@ -8,9 +8,11 @@ func main() {
 	var year int
 	fmt.Println("Введите год ")
 	fmt.Scan(&year)
-	if year%4 == 0 {
-		fmt.Println("Год висококосный ")
-	} else if year%4 != 0 {
-		fmt.Println("Год не висококосный ")
+	if year%4 != 0 {
+		fmt.Println("Год невисокосный ")
+	} else if year%100 != 0 {
+		fmt.Println("Год високосный ")
+	} else if year%100 == 0 && year%400 == 0 {
+		fmt.Println("Год високосный ")
 	}
 }
